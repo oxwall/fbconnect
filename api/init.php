611e8738d6336fe27986adc9afff101f1d188a20
Azazel <a_z_a_z_e_l@bk.ru>
2016-07-18 17:48:39 +0300

    Fix Android facebook connect

--- api/init.php
+++ api/init.php
@@ -1,4 +1,18 @@
 <?php
 
+$plugin = OW::getPluginManager()->getPlugin('fbconnect');
+
+function FBCONNECT_Autoloader( $className )
+{
+    if ( strpos($className, 'FBCONNECT_FC_') === 0 )
+    {
+        $file = OW::getPluginManager()->getPlugin('fbconnect')->getRootDir() . DS . 'classes' . DS . 'converters.php';
+        require_once $file;
+
+        return true;
+    }
+}
+spl_autoload_register('FBCONNECT_Autoloader');
+
 $eventHandler = new FBCONNECT_CLASS_EventHandler();
 $eventHandler->genericInit();
